9255acd10655876331840bfa1ea54c42ec65500e
em-s-h <soulinity.h@gmail.com>
2023-10-02 11:07:38 -0300

    Removed the css logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use gdk::Display;
-use gtk::{gdk, glib, prelude::*, CssProvider};
+use gtk::{glib, prelude::*};
 // use std::process::Command;
 
 /// Module for building ui elements
@@ -11,22 +10,8 @@ fn main() -> glib::ExitCode {
     // verify_xdelta();
 
     let app = adw::Application::builder().application_id(APP_ID).build();
-    app.connect_startup(|_| load_css());
     app.connect_activate(ui::build_ui);
     app.run()
-}
-
-fn load_css() {
-    // Load the CSS file and add it to the provider
-    let provider = CssProvider::new();
-    provider.load_from_data(include_str!("style.css"));
-
-    // Add the provider to the default screen
-    gtk::style_context_add_provider_for_display(
-        &Display::default().expect("Could not connect to a display."),
-        &provider,
-        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-    );
 }
 
 // fn verify_xdelta() {
