c3ec195717c3b5333cd57ce9b31b095342e68dea
Emilly M.S. <soulinity.h@gmail.com>
2023-10-14 17:48:56 -0300

    Warn user xdelta3 was not found

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use gtk::{glib, prelude::*};
-// use std::process::Command;
+use gtk::{glib, prelude::*, ApplicationWindow, MessageType};
+use std::{
+    process::{self, Command},
+    rc::Rc,
+};
 
 /// Module for building ui elements
 mod ui;
@@ -7,16 +10,36 @@ mod ui;
 const APP_ID: &str = "com.github.em-s-h.xdelta-lui";
 
 fn main() -> glib::ExitCode {
-    // verify_xdelta();
-
     let app = adw::Application::builder().application_id(APP_ID).build();
     app.connect_activate(ui::build_ui);
+    app.connect_startup(verify_xdelta);
     app.run()
 }
 
-// fn verify_xdelta() {
-//     Command::new("xdelta3")
-//         .arg("-q")
-//         .spawn()
-//         .expect("Xdelta3 is not installed! Please install it!");
-// }
+fn verify_xdelta(app: &adw::Application) {
+    let status = Command::new("xdelta3").arg("-q").status();
+
+    if let Ok(s) = status {
+        if s.success() {
+            return;
+        }
+    }
+
+    let message = format!(
+        "xdelta3 was not found! Please make sure it is installed. \n{}",
+        status.unwrap_err().to_string()
+    );
+    let window = Rc::new(
+        ApplicationWindow::builder()
+            .application(app)
+            .title("XDelta - LUI")
+            .build(),
+    );
+
+    let dialog = ui::build_dialog(Rc::clone(&window), MessageType::Error, &message);
+    dialog.connect_response(|obj, _| {
+        obj.destroy();
+        process::exit(1);
+    });
+    dialog.show();
+}
